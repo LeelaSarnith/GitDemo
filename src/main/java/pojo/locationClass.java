de4d5ebf32fa1ba4363ba33b76960cbbc8e98767
LeelaSarnith <leelavathim05@gmail.com>
2025-06-14 23:49:02 +0530

    changes in develop branch

--- src/main/java/pojo/locationClass.java
+++ src/main/java/pojo/locationClass.java
@@ -5,13 +5,24 @@ public class locationClass {
 
 		
 		private double lat;
+		private double let;
 		
+		public double getLet() {
+			return let;
+		}
+
+		public void setLet(double let) {
+			this.let = let;
+		}
+
 		public double getLat() {
 			return lat;
 		}
 
 		public void setLat(double lat) {
 			this.lat = lat;
+			System.out.println(lat);
+			System.out.println("Git");
 			System.out.println(lat);
 			System.out.println("Git");
 		}
