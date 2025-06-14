042baf886e10a4113fcab72626c8322cad09ecb1
LeelaSarnith <leelavathim05@gmail.com>
2025-06-14 23:15:59 +0530

    this is commit from gitdemo

--- src/main/java/pojo/locationClass.java
+++ src/main/java/pojo/locationClass.java
@@ -12,6 +12,7 @@ public class locationClass {
 
 		public void setLat(double lat) {
 			this.lat = lat;
+			System.out.println(lat);
 		}
 
 		public double getlng() {
@@ -20,6 +21,7 @@ public class locationClass {
 
 		public void setlng(double lng) {
 			this.lng = lng;
+			System.out.println(lng);
 		}
 
 		private double lng;
