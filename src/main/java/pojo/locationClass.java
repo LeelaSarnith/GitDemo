86e0065305f1a02760b5366ef5ae234c6588d2a6
LeelaSarnith <leelavathim05@gmail.com>
2025-06-14 23:34:18 +0530

    changes made by git

--- src/main/java/pojo/locationClass.java
+++ src/main/java/pojo/locationClass.java
@@ -13,6 +13,7 @@ public class locationClass {
 		public void setLat(double lat) {
 			this.lat = lat;
 			System.out.println(lat);
+			System.out.println("Git");
 		}
 
 		public double getlng() {
@@ -22,6 +23,7 @@ public class locationClass {
 		public void setlng(double lng) {
 			this.lng = lng;
 			System.out.println(lng);
+			System.out.println("Git lng");
 		}
 
 		private double lng;
